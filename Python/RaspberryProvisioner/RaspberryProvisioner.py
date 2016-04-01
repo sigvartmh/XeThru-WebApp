1390d59181c71081f4c7dbb3c4e25653ff8d857e
sigvartmh <sigvart.m@gmail.com>
2016-04-01 15:01:05 +0200

    Minor bugfixing

--- Python/RaspberryProvisioner/RaspberryProvisioner.py
+++ Python/RaspberryProvisioner/RaspberryProvisioner.py
@@ -56,6 +56,7 @@ class RaspberryProvisioner:
         #service isc-dhcp-server stop
     
     def enable(self):
+        self.reset_interfaces()
         self.dhcp_service("restart")
         self.hostapd_service("restart")
         self.reset_interfaces()

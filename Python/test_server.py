67c5ed488c2cb23019f559720da7dd1c536aab0e
Sigvart M. Hovland <Sigvart.m@gmail.com>
2016-04-22 12:40:30 +0000

    Added test server with no AP

---
+++ Python/test_server.py
@@ -0,0 +1,47 @@
+import subprocess, json, os, jinja2
+from flask import Flask, jsonify, render_template, url_for, send_from_directory, request, abort
+from RaspberryProvisioner import RaspberryProvisioner as RP
+app = Flask(__name__)
+ap  = RP()
+
+@app.route('/')
+def index():
+    url_for('static', filename='style.css')
+    url_for('static', filename='icons.css')
+    url_for('static', filename='materialize.min.css')
+    url_for('static', filename='jquery.min.js')
+    url_for('static', filename='materialize.min.js')
+    url_for('static', filename='angular.min.js')
+    url_for('static', filename='angular.map.js')
+    url_for('static', filename='app.js')
+    return render_template('index.html')
+
+@app.route('/wlan/api/scan', methods=['GET'])
+def list_wifi():
+    res = ap.scan()
+    return jsonify(res)
+
+@app.route('/font/roboto/<path:filename>')
+def send_font(filename):
+    print send_from_directory('font/roboto/', filename)
+    return send_from_directory('font/roboto/', filename)
+
+@app.route('/font/<path:path>')
+def send_test(path):
+    return "test:" + path
+
+@app.route('/wlan/api/connect', methods=['POST'])
+def setup_wifi():
+    if not request.json or not 'ssid' in request.json:
+        abort(400)
+    ap.connect_wifi(request.json)
+    response = { 'status': "sucess"}
+    return jsonify(response)
+
+@app.route('/wlan/api/connectivity',methods=['GET'])
+def check_connectivity():
+    return jsonify({'status' : ap.check_connectivity()})
+
+if __name__ == '__main__':
+    #Start flask server
+    app.run(host="0.0.0.0",port=80,debug=True)

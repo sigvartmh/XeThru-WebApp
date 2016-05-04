2170c51a4068fd55b431c196cab2cb51a92d7376
sigvartmh <sigvart.m@gmail.com>
2016-05-04 04:14:49 +0200

    Improved install script

--- Python/install.sh
+++ Python/install.sh
@@ -5,9 +5,19 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
+#Setup Path and command for running application
+base=$(pwd)
+app="wifi_ap.py"
+#Change this to >> file to log output of app data
+command="/usr/bin/python ${base}/${app} &> /dev/null"
+
+#install dependencies
 apt-get update
 for i in `cat dependencies.txt`;
 do apt-get install $i -y;
 done
 pip install -r requirements.txt
 apt-get remove python-dev
+
+#Install crontab with starting the app on reboot
+crontab -l | { cat; echo "@reboot ${command}";} | crontab -

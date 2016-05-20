1e6bbe63750712d5016f902621fb4229a76cd83c
Sigvart M. Hovland <Sigvart.m@gmail.com>
2016-05-20 05:16:36 +0000

    Small changes

--- Python/wifi_ap.py
+++ Python/wifi_ap.py
@@ -68,7 +68,7 @@ def check_connection():
 
 if __name__ == '__main__':
     if(check_connection()):
-        app.run(host="0.0.0.0",port=80,debug=False)
+        app.run(host="0.0.0.0",port=80,debug=True)
     else:
         #Sets up the necessary config files for access point mode
         ap.setup()
